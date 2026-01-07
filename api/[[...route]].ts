cf8d6082d5149933c79881a9c0667bb820da8e0a
Pramod Surya <napramod.surya@gmail.com>
2026-01-07 17:43:20 +0000

    Fix API handler imports

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -1,6 +1,9 @@
-import { Env } from '../../src/types';
-import index from '../../src/index';
+import { Env } from '../src/types';
+import index from '../src/index';
 
-export default async (req: Request, context: any) => {
-  return index.fetch(req, { TMDB_API_KEY: process.env.TMDB_API_KEY });
+export default async (req: Request) => {
+  const env: Env = {
+    TMDB_API_KEY: process.env.TMDB_API_KEY || ''
+  };
+  return index.fetch(req, env);
 };
